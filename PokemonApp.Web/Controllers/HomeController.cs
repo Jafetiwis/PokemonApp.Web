3ddad9c6cfaf6d764e2cdeb159b9851151ba9c27
Jafet <jafetalberto06@gmail.com>
2026-08-13 00:43:29 -0600

    Implementación funcionalidad para exportar listado a Excel

--- PokemonApp.Web/Controllers/HomeController.cs
+++ PokemonApp.Web/Controllers/HomeController.cs
@@ -2,6 +2,7 @@ using System.Diagnostics;
 using Microsoft.AspNetCore.Mvc;
 using PokemonApp.Web.Models;
 using PokemonApp.Web.Services;
+using ClosedXML.Excel;
 
 namespace PokemonApp.Web.Controllers
 {
@@ -41,6 +42,50 @@ namespace PokemonApp.Web.Controllers
                 pokemones = response.Results;
             }
             return View(pokemones);
+        }
+
+        public async Task<IActionResult> ExportarExcel(string? nombre, string? tipo, int offset = 0)
+        {
+            List<PokemonDto> pokemones = new List<PokemonDto>();
+
+            if (!string.IsNullOrEmpty(nombre))
+            {
+                var response = await _pokemonService.GetPokemonsAsync(1500, 0);
+                pokemones = response.Results.Where(p => p.Name.Contains(nombre.ToLower())).Skip(offset).Take(20).ToList();
+            }
+            else
+            {
+                var response = await _pokemonService.GetPokemonsAsync(20, offset);
+                pokemones = response.Results;
+            }
+
+            using (var workbook = new XLWorkbook())
+            {
+                var worksheet = workbook.Worksheets.Add("Pokédex");
+                var currentRow = 1;
+
+                worksheet.Cell(currentRow, 1).Value = "Número (ID)";
+                worksheet.Cell(currentRow, 2).Value = "Nombre del Pokémon";
+                worksheet.Cell(currentRow, 1).Value = "Enlace de Imagen";
+                worksheet.Row(1).Style.Font.Bold = true;
+
+                foreach (var pokemon in pokemones)
+                {
+                    currentRow++;
+                    worksheet.Cell(currentRow, 1).Value = pokemon.Id;
+                    worksheet.Cell(currentRow, 2).Value = char.ToUpper(pokemon.Name[0]) + pokemon.Name.Substring(1);
+                    worksheet.Cell(currentRow, 3).Value = pokemon.ImageUrl;
+                }
+
+                worksheet.Columns().AdjustToContents();
+
+                using (var stream = new MemoryStream())
+                {
+                    workbook.SaveAs(stream);
+                    var content = stream.ToArray();
+                    return File(content, "application/vnd.openxmlformats-officedocument.spreadsheetml.sheet", "Pokemones_Filtrados.xlsx");
+                }
+            }
         }
 
         public IActionResult Privacy()
